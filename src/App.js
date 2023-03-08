0229b365da3f0f6128738c53c1f1f5b8a66767a3
gweryf <priyam2007sp@gmail.com>
2023-03-08 22:44:08 +0530

    practiced states a bit lol

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(4)
+  const [theme, setTheme] = useState('lmao')
+
+  function decrementCount(){
+    setCount(count-1)
+  }
+
+  function incrementCount() {
+    setCount(count+1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" flex gap-1 justify-center items-center">
+      <button className=' text-3xl bg-slate-400 rounded-lg p-1' onClick={decrementCount}>-</button>
+      <span className=' text-3xl'>{count}</span>
+      <span className=' text-3xl'>{theme}</span>
+      <button className=' text-3xl bg-slate-400 rounded-lg p-1' onClick={incrementCount}>+</button>
     </div>
   );
 }
