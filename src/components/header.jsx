c4d2e9d7a1c0b41ee0282cae0cc5284714285302
gweryf <priyam2007sp@gmail.com>
2023-03-19 14:51:42 +0530

    added header to the website

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+
+function Header(){
+    return(
+        <div className=" font-bold text-4xl bg-gray-200 p-5 text-center drop-shadow-lg">
+            CV-Builder
+        </div>
+    )
+}
+
+export default Header
