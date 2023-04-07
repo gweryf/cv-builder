10df4f7654f62bd73822fe0ece6c48b013fd80ee
gweryf <priyam2007sp@gmail.com>
2023-04-07 15:21:56 +0530

    styled the form

---
+++ src/components/educational.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+function Educational() {
+    return(
+        <>
+            <input
+                type="text"
+                  
+            />
+        </>
+    )
+}
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -15,76 +15,226 @@ function Form({addContract}) {
     }
 
     return(
-        <>
+        <div className=" border text-center p-4 max-w-xl max-h-xl shadow-lg my-7 bg-gray-50">
             <form action="" onSubmit={handleSubmit}>
+
+                {/* personal details */}
+                <h3 className=" text-xl">Personal Details</h3>
                 <input
                     name="name"
                     type="text"
                     placeholder="Name"
                     onChange={handleChange}
-                    className=" border-solid border-black border-2"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none"
                 /><br/>
                 <input
                     name="phone"
                     type="number"
                     onChange={handleChange}
                     placeholder="Phone Number"
-                    className=" border-solid border-black border-2"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
                 /><br/>
                 <input
                     name="email"
                     type="email"
                     onChange={handleChange}
                     placeholder="E-MAIL"
-                    className=" border-solid border-black border-2"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
                 /><br/>
                 <input
                     name="linkedin"
                     type="text"
                     onChange={handleChange}
                     placeholder="LinkedIn URL"
-                    className=" border-solid border-black border-2"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
                 /><br/>
                 <input
                     name="github"
                     type="text"
                     placeholder="GitHub URL"
                     onChange={handleChange}
-                    className=" border-solid border-black border-2"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
                 /><br/>
+
+                {/* Work Experience */}
+                <h3 className=" text-xl">Experience</h3>
                 <input
-                    name="company-name"
+                    name="company-name1"
                     type="text"
                     placeholder="Company Name"
                     onChange={handleChange}
-                    className=" border-solid border-black border-2"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
                 /><br/>
                 <input
-                    name="company-position"
+                    name="company-position1"
                     type="text"
                     onChange={handleChange}
                     placeholder="Position"
-                    className=" border-solid border-black border-2"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
                 /><br/>
                 <input
-                    name="Start Date"
-                    type="number"
+                    name="Start-Date1"
+                    type="date"
                     placeholder="Start Date"
                     onChange={handleChange}
-                    className=" border-solid border-black border-2"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
                 /><br/>
                 <input
-                    name="End Date"
-                    type="number"
+                    name="End-Date1"
+                    type="date"
                     placeholder="End Date"
                     onChange={handleChange}
-                    className=" border-solid border-black border-2"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
                 /><br/>
-                <button type="submit" className=" rounded-lg border-2 border-black border-solid p-2">
+                <input
+                    name="company-name2"
+                    type="text"
+                    placeholder="Company Name"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="company-position2"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Position"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="Start-Date2"
+                    type="date"
+                    placeholder="Start Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="End-Date2"
+                    type="date"
+                    placeholder="End Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="company-name3"
+                    type="text"
+                    placeholder="Company Name"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="company-position3"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Position"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="Start-Date3"
+                    type="date"
+                    placeholder="Start Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="End-Date3"
+                    type="date"
+                    placeholder="End Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="company-name4"
+                    type="text"
+                    placeholder="Company Name"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="company-position4"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Position"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="Start-Date4"
+                    type="date"
+                    placeholder="Start Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-red-600"
+                /><br/>
+                <input
+                    name="End-Date4"
+                    type="date"
+                    placeholder="End Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+
+                {/* Education Details   */}
+                <h3 className=" text-xl">Education</h3>
+                <input
+                    name="university-name1"
+                    type="text"
+                    placeholder="University Name"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="degree1"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Degree"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="course-start1"
+                    type="text"
+                    placeholder="Start Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="course-end1"
+                    type="text"
+                    placeholder="End Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+
+                <input
+                    name="university-name2"
+                    type="text"
+                    placeholder="University Name"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="degree2"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Degree"
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="course-start2"
+                    type="text"
+                    placeholder="Start Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <input
+                    name="course-end2"
+                    type="text"
+                    placeholder="End Date"
+                    onChange={handleChange}
+                    className=" my-1 w-full border border-gray-600 px-3 py-1 rounded-lg shadow-sm focus:outline-none focus:border-gray-800"
+                /><br/>
+                <button type="submit" className=" my-2 rounded-lg border border-black px-5 py-1 transform transition duration-300 hover:scale-110 hover:bg-gray-300">
                     Submit
                 </button>
             </form>
-        </>
+        </div>
     )
 }
 
