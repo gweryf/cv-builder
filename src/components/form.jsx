431dd54f8eb3a964eb72fb4bcd0bcac77d859830
gweryf <priyam2007sp@gmail.com>
2023-04-07 09:34:54 +0530

    added states to store form data

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,68 +2,90 @@ import React from "react";
 import { useState } from "react";
 
 function Form() {
+    const [details, UpdateDetails] = useState({})
+
+    function handleChange(event) {
+        UpdateDetails({...details, [event.target.name]:event.target.value})
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(details);
+    }
+
     return(
         <>
-            <input
-                name="name"
-                type="text"
-                placeholder="Name"
-                className=" border-solid border-black border-2"
-            /><br/>
-            <input
-                name="phone"
-                type="number"
-                placeholder="Phone Number"
-                className=" border-solid border-black border-2"
-            /><br/>
-            <input
-                name="email"
-                type="email"
-                placeholder="E-MAIL"
-                className=" border-solid border-black border-2"
-            /><br/>
-            <input
-                name="linkedin"
-                type="text"
-                placeholder="LinkedIn URL"
-                className=" border-solid border-black border-2"
-            /><br/>
-            <input
-                name="github"
-                type="text"
-                placeholder="GitHub URL"
-                className=" border-solid border-black border-2"
-            /><br/>
-            <input
-                name="company-name"
-                type="text"
-                placeholder="Company Name"
-                className=" border-solid border-black border-2"
-            /><br/>
-            <input
-                name="company-position"
-                type="text"
-                placeholder="Position"
-                className=" border-solid border-black border-2"
-            /><br/>
-            <input
-                name="Start Date"
-                type="number"
-                placeholder="Start Date"
-                className=" border-solid border-black border-2"
-            /><br/>
-            <input
-                name="End Date"
-                type="number"
-                placeholder="End Date"
-                className=" border-solid border-black border-2"
-            /><br/>
-            <button
-                type="submit"
-                className=" rounded-lg border-2 border-black border-solid p-2"
-            >
-                Submit
-            </button>
+            <form action="" onSubmit={handleSubmit}>
+                <input
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                    onChange={handleChange}
+                    className=" border-solid border-black border-2"
+                /><br/>
+                <input
+                    name="phone"
+                    type="number"
+                    onChange={handleChange}
+                    placeholder="Phone Number"
+                    className=" border-solid border-black border-2"
+                /><br/>
+                <input
+                    name="email"
+                    type="email"
+                    onChange={handleChange}
+                    placeholder="E-MAIL"
+                    className=" border-solid border-black border-2"
+                /><br/>
+                <input
+                    name="linkedin"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="LinkedIn URL"
+                    className=" border-solid border-black border-2"
+                /><br/>
+                <input
+                    name="github"
+                    type="text"
+                    placeholder="GitHub URL"
+                    onChange={handleChange}
+                    className=" border-solid border-black border-2"
+                /><br/>
+                <input
+                    name="company-name"
+                    type="text"
+                    placeholder="Company Name"
+                    onChange={handleChange}
+                    className=" border-solid border-black border-2"
+                /><br/>
+                <input
+                    name="company-position"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Position"
+                    className=" border-solid border-black border-2"
+                /><br/>
+                <input
+                    name="Start Date"
+                    type="number"
+                    placeholder="Start Date"
+                    onChange={handleChange}
+                    className=" border-solid border-black border-2"
+                /><br/>
+                <input
+                    name="End Date"
+                    type="number"
+                    placeholder="End Date"
+                    onChange={handleChange}
+                    className=" border-solid border-black border-2"
+                /><br/>
+                <button
+                    type="submit"
+                    className=" rounded-lg border-2 border-black border-solid p-2"
+                >
+                    Submit
+                </button>
+            </form>
         </>
     )
 }
