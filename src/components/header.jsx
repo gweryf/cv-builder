f22b8d43d7fd40e9728a64f0d9129988d3309859
gweryf <priyam2007sp@gmail.com>
2023-04-09 09:19:46 +0530

    fixed the mobile view

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,7 +14,7 @@ function Header(){
         html2pdf().set(opt).from(element).save()
     }
     return(
-        <div className=" bg-gray-200 p-5 text-center drop-shadow-lg flex justify-around">
+        <div className=" bg-gray-200 p-5 text-center drop-shadow-lg md:flex justify-around">
             <div className=" font-bold text-4xl">CV-Builder</div>
             <button onClick={handleDownload} className=" my-2 rounded-lg border bg-black text-white border-black px-5 py-1 transform transition duration-300 hover:scale-110 hover:bg-gray-300 hover:text-black">
                 Download
