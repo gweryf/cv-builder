407acfd411dba44c8117f3ac9fc40f3ab7fe59d8
gweryf <priyam2007sp@gmail.com>
2023-04-09 09:01:55 +0530

    added download button to download the cv

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -8,101 +8,102 @@ import email from "../assets/email.png"
 function Canvas(props){
     return(
         <div className=" border p-4 w-full md:mx-20 self-start shadow-lg my-7">
-            
-            {/* Name */}
-            <div className=" text-4xl font-bold p-4 text-center">
-                {props.contracts.name}
-            </div>
+            <div id="element-to-print">
+                {/* Name */}
+                <div className=" text-4xl font-bold p-4 text-center">
+                    {props.contracts.name}
+                </div>
 
-            {/* Contact Details */}
-            <div className=" flex gap-2 items-center justify-center py-3  text-xs md:text-base">
-                <img src={phone} className=" w-7" alt="" />
-                {props.contracts.phone}
-                <a className=" cursor-pointer flex gap-2 items-center justify-center" href={props.contracts.linkedin}>
-                    <img src={linkedin} className=" w-5 h-5"/>
-                    LinkedIn
-                </a>
-                <a className=" cursor-pointer flex gap-2 items-center justify-center" href={props.contracts.github}>
-                    <img src={github} className=" w-7" alt="" />
-                    Github
-                </a>
-                <img src={email} className=" w-7 h-6" alt="" />
-                {props.contracts.email} 
-            </div>
+                {/* Contact Details */}
+                <div className=" flex gap-2 items-center justify-center py-3  text-xs md:text-base">
+                    <img src={phone} className=" w-7" alt="" />
+                    {props.contracts.phone}
+                    <a className=" cursor-pointer flex gap-2 items-center justify-center" href={props.contracts.linkedin}>
+                        <img src={linkedin} className=" w-5 h-5"/>
+                        LinkedIn
+                    </a>
+                    <a className=" cursor-pointer flex gap-2 items-center justify-center" href={props.contracts.github}>
+                        <img src={github} className=" w-7" alt="" />
+                        Github
+                    </a>
+                    <img src={email} className=" w-7 h-6" alt="" />
+                    {props.contracts.email} 
+                </div>
 
-            {/* Description */}
-            <div className=" text-xl font-semibold py-3 border-b border-gray-700">
-                Description
-            </div>
-            <div className=" py-2 text-sm">
-                {props.contracts.descrp}
-            </div>
+                {/* Description */}
+                <div className=" text-xl font-semibold py-3 border-b border-gray-700">
+                    Description
+                </div>
+                <div className=" py-2 text-sm">
+                    {props.contracts.descrp}
+                </div>
 
-            {/* Experience */}
-            <div className=" text-xl font-semibold py-3 border-b border-gray-700">
-                Experience
-            </div>
-            <div className=" my-1">
-                <div className=" flex justify-between">
-                    <div className=" font-medium">{props.contracts.company1}</div>
-                    <div>{props.contracts.startDate1} - {props.contracts.endDate1}</div>
+                {/* Experience */}
+                <div className=" text-xl font-semibold py-3 border-b border-gray-700">
+                    Experience
                 </div>
-                <div className=" text-sm">
-                    {props.contracts.position1}
+                <div className=" my-1">
+                    <div className=" flex justify-between">
+                        <div className=" font-medium">{props.contracts.company1}</div>
+                        <div>{props.contracts.startDate1} - {props.contracts.endDate1}</div>
+                    </div>
+                    <div className=" text-sm">
+                        {props.contracts.position1}
+                    </div>
                 </div>
-            </div>
 
-            <div className=" my-1">
-                <div className=" flex justify-between">
-                    <div className=" font-medium">{props.contracts.company2}</div>
-                    <div>{props.contracts.startDate2} - {props.contracts.endDate2}</div>
+                <div className=" my-1">
+                    <div className=" flex justify-between">
+                        <div className=" font-medium">{props.contracts.company2}</div>
+                        <div>{props.contracts.startDate2} - {props.contracts.endDate2}</div>
+                    </div>
+                    <div>
+                        {props.contracts.position2}
+                    </div>
                 </div>
-                <div>
-                    {props.contracts.position2}
-                </div>
-            </div>
 
-            <div className=" my-1">
-                <div className=" flex justify-between">
-                    <div className=" font-medium">{props.contracts.company3}</div>
-                    <div>{props.contracts.startDate3} - {props.contracts.endDate3}</div>
+                <div className=" my-1">
+                    <div className=" flex justify-between">
+                        <div className=" font-medium">{props.contracts.company3}</div>
+                        <div>{props.contracts.startDate3} - {props.contracts.endDate3}</div>
+                    </div>
+                    <div>
+                        {props.contracts.position3}
+                    </div>
                 </div>
-                <div>
-                    {props.contracts.position3}
+                <div className=" my-1">
+                    <div className=" flex justify-between">
+                        <div className=" font-medium">{props.contracts.company4}</div>
+                        <div>{props.contracts.startDate4} - {props.contracts.endDate4}</div>
+                    </div>
+                    <div>
+                        {props.contracts.position4}
+                    </div>
                 </div>
-            </div>
-            <div className=" my-1">
-                <div className=" flex justify-between">
-                    <div className=" font-medium">{props.contracts.company4}</div>
-                    <div>{props.contracts.startDate4} - {props.contracts.endDate4}</div>
-                </div>
-                <div>
-                    {props.contracts.position4}
-                </div>
-            </div>
 
-            {/* Education */}
-            <div className=" text-xl font-semibold py-3 border-b border-gray-700">
-                Education
-            </div>
+                {/* Education */}
+                <div className=" text-xl font-semibold py-3 border-b border-gray-700">
+                    Education
+                </div>
 
-            <div className=" my-1">
-                <div className=" flex justify-between">
-                    <div className=" font-medium">{props.contracts.university1}</div>
-                    <div>{props.contracts.coursestart1} - {props.contracts.courseend1}</div>
+                <div className=" my-1">
+                    <div className=" flex justify-between">
+                        <div className=" font-medium">{props.contracts.university1}</div>
+                        <div>{props.contracts.coursestart1} - {props.contracts.courseend1}</div>
+                    </div>
+                    <div>
+                        {props.contracts.degree1}
+                    </div>
                 </div>
-                <div>
-                    {props.contracts.degree1}
-                </div>
-            </div>
 
-            <div className=" my-1">
-                <div className=" flex justify-between">
-                    <div className=" font-medium">{props.contracts.university2}</div>
-                    <div>{props.contracts.coursestart2} - {props.contracts.courseend2}</div>
-                </div>
-                <div>
-                    {props.contracts.degree2}
+                <div className=" my-1">
+                    <div className=" flex justify-between">
+                        <div className=" font-medium">{props.contracts.university2}</div>
+                        <div>{props.contracts.coursestart2} - {props.contracts.courseend2}</div>
+                    </div>
+                    <div>
+                        {props.contracts.degree2}
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import { useState } from "react";
 
 function Header(){
+    function handleDownload() {
+        let element = document.getElementById('element-to-print')
+        let opt = {
+            margin:       1,
+            filename:     'resume.pdf',
+            image:        { type: 'jpeg', quality: 1 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+        html2pdf().set(opt).from(element).save()
+    }
     return(
-        <div className=" font-bold text-4xl bg-gray-200 p-5 text-center drop-shadow-lg  ">
-            CV-Builder
+        <div className=" bg-gray-200 p-5 text-center drop-shadow-lg flex justify-around">
+            <div className=" font-bold text-4xl">CV-Builder</div>
+            <button onClick={handleDownload} className=" my-2 rounded-lg border bg-black text-white border-black px-5 py-1 transform transition duration-300 hover:scale-110 hover:bg-gray-300 hover:text-black">
+                Download
+            </button>
         </div>
     )
 }
